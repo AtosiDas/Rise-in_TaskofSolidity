1b6c665ac8a5863d47e3aaca9fe20dea068555ad
Atosi Das <115734233+AtosiDas@users.noreply.github.com>
2024-02-15 18:17:58 +0530

    Write the code

--- Task1.sol
+++ Task1.sol
@@ -1,1 +1,29 @@
+//SPDX-License-Identifier: MIT
 
+pragma solidity >= 0.8.0;
+contract Task1{
+    struct Counter{
+        string description;
+        uint256 value;
+    }
+    Counter counter;
+    address Owner;
+    constructor(string memory _des, uint256 _value) {
+        Owner = msg.sender;
+        counter.value = _value;
+        counter.description = _des;
+    }
+    modifier OnlyOwner{
+        require(msg.sender == Owner,"You are not allowed!");
+        _;
+    }
+    function Increment_counter() public OnlyOwner{
+        counter.value += 1;
+    }
+    function Decrement_counter() public OnlyOwner{
+        counter.value -= 1;
+    }
+    function GetValue_counter() public view returns(uint256){
+        return counter.value;
+    }
+}
