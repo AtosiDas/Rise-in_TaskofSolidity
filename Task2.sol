5c085e15620841689ff848ba65d046f94d9f1ad8
Atosi Das <115734233+AtosiDas@users.noreply.github.com>
2024-02-16 20:00:04 +0530

    Add all initial code for Task2.sol

--- Task2.sol
+++ Task2.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract ProposalContract {
+    // Our contract code
+    struct Proposal {
+        string description; // Description of the proposal
+        uint256 approve; // Number of approve votes
+        uint256 reject; // Number of reject votes
+        uint256 pass; // Number of pass votes
+        uint256 total_vote_to_end; // When the total votes in the proposal reaches this limit, proposal ends
+        bool current_state; // This shows the current state of the proposal, meaning whether if passes of fails
+        bool is_active; // This shows if others can vote to our contract
+    }
+    mapping(uint256 => Proposal) proposal_history; // Recordings of previous proposals
 
+
+}
